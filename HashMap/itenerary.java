5e587a7cce68ec3ad1c0c764e8aaaff36b55ae7e
rohan472000 <anand00rohan@gmail.com>
2023-01-17 00:02:35 +0530

    committed from intellij

---
+++ HashMap/itenerary.java
@@ -0,0 +1,34 @@
+package HashMap;
+
+import java.util.HashMap;
+
+public class itenerary {
+    public static String getStart(HashMap<String,String> tick){
+        HashMap<String,String> revmap = new HashMap<>();
+        for (String key:tick.keySet()) {
+            revmap.put(tick.get(key),key);
+        }
+        for (String key: tick.keySet()) {
+            if (revmap.containsKey(key))
+                return key;
+        }
+        return null;
+    }
+    public static void main(String[] args) {
+        HashMap<String,String> map = new HashMap<>();
+        map.put( "chennai","bengaluru");
+        map.put("mumbai","delhi");
+        map.put("goa","chennai");
+        map.put("delhi","goa");
+
+        String start = getStart(map);
+        //System.out.println(start+"-----");
+        while (map.containsKey(start)){
+            System.out.println("-----");
+            System.out.println(start);
+            start = map.get(start);
+        }
+        System.out.println("-------");
+        System.out.println(start);
+    }
+}
---
+++ HashMap/union_of_two_arrays.java
@@ -0,0 +1,23 @@
+package HashMap;
+
+import java.util.HashSet;
+
+public class union_of_two_arrays {
+    public static int printUnion(int[] a,int[] b){
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < a.length; i++) {
+            set.add(a[i]);
+        }
+        for (int i = 0; i < b.length; i++) {
+            set.add(b[i]);
+        }
+        System.out.println(set); // print all elements present in hashset
+        return set.size();
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,2,5,1,3,1,5,1};
+        int[] nums1 = {9,5,5,8};
+        System.out.println(printUnion(nums,nums1));
+    }
+}
