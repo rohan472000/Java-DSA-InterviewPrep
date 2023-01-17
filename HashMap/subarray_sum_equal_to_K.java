12209da556432e8d1d238b5fb2af9e16d2fc2abd
rohan472000 <anand00rohan@gmail.com>
2023-01-18 00:27:30 +0530

    committed from intellij

--- HashMap/subarray_sum_equal_to_K.java
+++ HashMap/subarray_sum_equal_to_K.java
@@ -3,6 +3,20 @@ package HashMap;
 import java.util.HashMap;
 
 public class subarray_sum_equal_to_K {
+    public static int subsum(int[] a,int k){
+        int ans =0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,1);
+        int sum=0;
+        for (int i = 0; i < a.length; i++) {
+            sum+=a[i];
+            if (map.containsKey(sum-k)){
+                ans+=map.get(sum-k);
+            }
+            map.put(sum, map.getOrDefault(sum,0)+1);
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         int[] arr = {10,2,-2,-20,10};
         int k = -10;
@@ -16,7 +30,7 @@ public class subarray_sum_equal_to_K {
             sum+=arr[i];
 
             if (map.containsKey(sum-k)){
-                System.out.println(sum-k);
+                //System.out.println(sum-k);
                 ans+=map.get(sum-k);
             }
             if (map.containsKey(sum)){
