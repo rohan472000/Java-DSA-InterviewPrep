3bcf43df0b737fee79f68a9611f25318a04faf50
rohan472000 <anand00rohan@gmail.com>
2023-01-21 20:50:03 +0530

    committed from intellij

--- Graphs/graph_apna.java
+++ Graphs/graph_apna.java
@@ -6,9 +6,11 @@ public class graph_apna {
     static class Edge{
         int src;
         int dest;
-        public Edge(int s,int d){
+        int wt;
+        public Edge(int s,int d, int w){
             this.src = s;
             this.dest=  d;
+            this.wt = w;
         }
     }
     public  static void createGraph(ArrayList<Edge> graph[]){
@@ -16,17 +18,17 @@ public class graph_apna {
             graph[i] = new ArrayList<Edge>();
         }
 
-        graph[0].add(new Edge(0,2));
+        graph[0].add(new Edge(0,2,2));
 
-        graph[1].add(new Edge(1,2));
-        graph[1].add(new Edge(1,3));
+        graph[1].add(new Edge(1,2,10));
+        graph[1].add(new Edge(1,3,0));
 
-        graph[2].add(new Edge(2,0));
-        graph[2].add(new Edge(2,1));
-        graph[2].add(new Edge(2,3));
+        graph[2].add(new Edge(2,0,2));
+        graph[2].add(new Edge(2,1,10));
+        graph[2].add(new Edge(2,3,-1));
 
-        graph[3].add(new Edge(3,1));
-        graph[3].add(new Edge(3,2));
+        graph[3].add(new Edge(3,1,0));
+        graph[3].add(new Edge(3,2,-1));
     }
     public static void main(String[] args) {
    int V = 4;
@@ -37,7 +39,7 @@ public class graph_apna {
         //print 2's neighbour
         for (int i = 0; i < graph[2].size(); i++) {
             Edge e = graph[2].get(i);
-            System.out.println(e.dest+"  ");
+            System.out.println(e.dest+" has weight "+e.wt);
         }
     }
 }
