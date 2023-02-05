bc66bb0b9dd0de8d3924bf901e975211f17dee71
rohan472000 <anand00rohan@gmail.com>
2023-02-05 15:24:40 +0530

    committed from intellij

---
+++ Stack/gameOfTwoStacks.java
@@ -0,0 +1,13 @@
+package Stack;
+
+public class gameOfTwoStacks {
+    public static void main(String[] args) {
+      int[]  a = {1,2,3,4,5};
+        int[]  b = {6,7,8,9};
+    }
+    public int solution(int max,int[] a,int[] b){
+        int sum = 0;
+        int count = 0;
+
+    }
+}
