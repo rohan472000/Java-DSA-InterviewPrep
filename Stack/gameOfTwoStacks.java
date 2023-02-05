cc40289b948b9b9d4baaf2377a893f4301fbc29b
rohan472000 <anand00rohan@gmail.com>
2023-02-05 23:39:52 +0530

    committed from intellij

--- Stack/gameOfTwoStacks.java
+++ Stack/gameOfTwoStacks.java
@@ -5,9 +5,10 @@ public class gameOfTwoStacks {
       int[]  a = {1,2,3,4,5};
         int[]  b = {6,7,8,9};
     }
-    public int solution(int max,int[] a,int[] b){
-        int sum = 0;
-        int count = 0;
-
+    public int solution(int x,int[] a,int[] b){
+        return twostack(x,a,b,0,0)-1;
     }
+     public static int twostack(int x,int[] a,int[] b,int sum,int count){
+        if (sum > x) return count;
+     }
 }
