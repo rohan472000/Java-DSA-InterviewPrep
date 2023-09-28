6fa87dddb10b58d4767b4968350b8f8cdb0bd95b
Piyush kumar <90105030+whitehawk0910@users.noreply.github.com>
2023-09-28 17:58:01 +0530

    Update dijkstra.java

--- dijkstra.java
+++ dijkstra.java
@@ -1,10 +1,11 @@
-import java.util.Arrays;
+import java.util.Arrays;// Importing the Arrays class from java.util package to use utility methods for array operations
 
 public class DijkstraAlgorithm {
 
     private static final int INFINITY = Integer.MAX_VALUE;
 
     public static void main(String[] args) {
+        // Example graph represented as an adjacency matrix
         int[][] graph = {
                 {0, 4, 0, 0, 0, 0, 0, 8, 0},
                 {4, 0, 8, 0, 0, 0, 0, 11, 0},
@@ -21,6 +22,12 @@ public class DijkstraAlgorithm {
         dijkstra(graph, startNode);
     }
 
+    /**
+     * Dijkstra's algorithm to find the shortest paths from a source node to all other nodes in a graph.
+     *
+     * @param graph      The adjacency matrix representing the graph.
+     * @param startNode  The starting node for the algorithm.
+     */
     private static void dijkstra(int[][] graph, int startNode) {
         int vertices = graph.length;
         int[] dist = new int[vertices];
@@ -44,6 +51,13 @@ public class DijkstraAlgorithm {
         printSolution(dist);
     }
 
+    /**
+     * Helper function to find the index of the vertex with the minimum distance value.
+     *
+     * @param dist    The array of distance values.
+     * @param visited The array indicating whether a node has been visited or not.
+     * @return The index of the minimum distance vertex.
+     */
     private static int minDistance(int[] dist, boolean[] visited) {
         int min = INFINITY;
         int minIndex = -1;
@@ -58,6 +72,11 @@ public class DijkstraAlgorithm {
         return minIndex;
     }
 
+    /**
+     * Helper function to print the solution, i.e., the shortest distances from the source vertex.
+     *
+     * @param dist The array of shortest distances.
+     */
     private static void printSolution(int[] dist) {
         System.out.println("Shortest distances from the source vertex:");
         for (int i = 0; i < dist.length; i++) {
@@ -65,3 +84,4 @@ public class DijkstraAlgorithm {
         }
     }
 }
+
