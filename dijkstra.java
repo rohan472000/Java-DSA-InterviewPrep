557fc400b73e5676f7c88a75d8f556848cd99d4b
Piyush kumar <90105030+whitehawk0910@users.noreply.github.com>
2023-09-29 11:12:42 +0530

    Update dijkstra.java

--- dijkstra.java
+++ dijkstra.java
@@ -1,12 +1,12 @@
-import java.util.Arrays;// Importing the Arrays class from java.util package to use utility methods for array operations
+import java.util.Arrays;
 
 public class DijkstraAlgorithm {
 
     private static final int INFINITY = Integer.MAX_VALUE;
 
     public static void main(String[] args) {
-        // Example graph represented as an adjacency matrix
-        int[][] graph = {
+        // Test Case 1
+        int[][] graph1 = {
                 {0, 4, 0, 0, 0, 0, 0, 8, 0},
                 {4, 0, 8, 0, 0, 0, 0, 11, 0},
                 {0, 8, 0, 7, 0, 4, 0, 0, 2},
@@ -17,16 +17,54 @@ public class DijkstraAlgorithm {
                 {8, 11, 0, 0, 0, 0, 1, 0, 7},
                 {0, 0, 2, 0, 0, 0, 6, 7, 0}
         };
+        int startNode1 = 0;
+        runDijkstra(graph1, startNode1);
 
-        int startNode = 0;
-        dijkstra(graph, startNode);
+        // Test Case 2
+        int[][] graph2 = {
+                {0, 3, 0, 0, 0, 0, 0, 4},
+                {3, 0, 1, 0, 0, 0, 0, 0},
+                {0, 1, 0, 8, 0, 0, 0, 0},
+                {0, 0, 8, 0, 2, 0, 0, 0},
+                {0, 0, 0, 2, 0, 6, 0, 0},
+                {0, 0, 0, 0, 6, 0, 7, 0},
+                {0, 0, 0, 0, 0, 7, 0, 9},
+                {4, 0, 0, 0, 0, 0, 9, 0}
+        };
+        int startNode2 = 2;
+        runDijkstra(graph2, startNode2);
+
+        // Test Case 3
+        int[][] graph3 = {
+                {0, 2, 4, 0, 0, 0},
+                {2, 0, 1, 7, 0, 0},
+                {4, 1, 0, 0, 3, 0},
+                {0, 7, 0, 0, 0, 1},
+                {0, 0, 3, 0, 0, 5},
+                {0, 0, 0, 1, 5, 0}
+        };
+        int startNode3 = 0;
+        runDijkstra(graph3, startNode3);
     }
 
     /**
-     * Dijkstra's algorithm to find the shortest paths from a source node to all other nodes in a graph.
-     *
-     * @param graph      The adjacency matrix representing the graph.
-     * @param startNode  The starting node for the algorithm.
+     * Run Dijkstra's algorithm on the given graph starting from the specified node.
+     * @param graph The graph represented as an adjacency matrix.
+     * @param startNode The starting node for the algorithm.
+     */
+    private static void runDijkstra(int[][] graph, int startNode) {
+        System.out.println("Graph:");
+        printGraph(graph);
+
+        System.out.println("\nShortest distances from Node " + startNode + ":");
+        dijkstra(graph, startNode);
+        System.out.println("-------------------------");
+    }
+
+    /**
+     * Implementation of Dijkstra's algorithm to find the shortest paths from a source node to all other nodes in a graph.
+     * @param graph The adjacency matrix representing the graph.
+     * @param startNode The starting node for the algorithm.
      */
     private static void dijkstra(int[][] graph, int startNode) {
         int vertices = graph.length;
@@ -53,8 +91,7 @@ public class DijkstraAlgorithm {
 
     /**
      * Helper function to find the index of the vertex with the minimum distance value.
-     *
-     * @param dist    The array of distance values.
+     * @param dist The array of distance values.
      * @param visited The array indicating whether a node has been visited or not.
      * @return The index of the minimum distance vertex.
      */
@@ -74,13 +111,22 @@ public class DijkstraAlgorithm {
 
     /**
      * Helper function to print the solution, i.e., the shortest distances from the source vertex.
-     *
      * @param dist The array of shortest distances.
      */
     private static void printSolution(int[] dist) {
-        System.out.println("Shortest distances from the source vertex:");
+        System.out.println("Shortest distances:");
         for (int i = 0; i < dist.length; i++) {
             System.out.println("To " + i + ": " + dist[i]);
+        }
+    }
+
+    /**
+     * Helper function to print the adjacency matrix representation of the graph.
+     * @param graph The graph represented as an adjacency matrix.
+     */
+    private static void printGraph(int[][] graph) {
+        for (int[] row : graph) {
+            System.out.println(Arrays.toString(row));
         }
     }
 }
