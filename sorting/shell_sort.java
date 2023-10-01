dab4d217e5e5d4004ebc647ddf5889584ef69584
sreshta <121309862+sreshtech@users.noreply.github.com>
2023-10-01 21:43:59 +0530

    Update shell_sort.java

--- sorting/shell_sort.java
+++ sorting/shell_sort.java
@@ -1,3 +1,18 @@
+/* This class demonstrates a Shell Sort algorithm.
+Description:
+Shell Sort is an efficient and adaptive sorting algorithm that is an extension of the Insertion Sort algorithm.
+It works by dividing the array into smaller subarrays and then sorting these subarrays using the Insertion Sort method. 
+The key idea behind Shell Sort is to compare and swap elements that are far apart, gradually reducing the gap between elements until the entire array is sorted.
+Basic Steps:
+1.Choose a gap sequence (commonly, it starts with n/2 where n is the length of the array) to determine the initial gap.
+2.Divide the array into subarrays of elements separated by the chosen gap.
+3.Apply Insertion Sort to each subarray independently to sort the elements within the subarray.
+4.Reduce the gap between elements and repeat steps 2 and 3 until the gap becomes 1.
+5.Finally, apply Insertion Sort with a gap of 1 to perform the final sorting, which is usually efficient because the array has been partially sorted by the previous steps.
+Shell Sort is known for its adaptive nature, as the choice of gap sequence can significantly affect its performance. 
+It has a time complexity that depends on the gap sequence used but often exhibits a better average-case performance compared to the simple Insertion Sort.
+* For more information, see:https://en.wikipedia.org/wiki/Shellsort */
+
 public class shell_sort {
 
     public static void shellSort(int[] arr) {
