af0dd2729dfce4f9afd6b96635d7bf2c528c86b5
shivamk23 <shivamdeveloper23@gmail.com>
2023-10-05 01:42:34 +0530

    update ImplementingGraphs.java file

--- Graphs/ImplementingGraphs.java
+++ Graphs/ImplementingGraphs.java
@@ -1,9 +1,10 @@
+
 import java.util.*;
 
-
+// Class to represent a graph
 class Graph {
-    private int V;
-    private Map<Integer, List<Integer>> adjacencyList; 
+    private int V; // Number of vertices
+    private Map<Integer, List<Integer>> adjacencyList; // Adjacency list
 
     // Constructor
     public Graph(int V) {
@@ -12,12 +13,19 @@ class Graph {
         for (int i = 0; i < V; i++) {
             adjacencyList.put(i, new ArrayList<>());
         }
-    } 
+    }
+
+    // Add an edge to the graph
+    // This method adds an undirected edge between vertices v and w
+    // For more details, see: https://en.wikipedia.org/wiki/Graph_(discrete_mathematics)
     public void addEdge(int v, int w) {
         adjacencyList.get(v).add(w);
         adjacencyList.get(w).add(v);
     }
+
     // Print the graph
+    // This method prints the adjacency list representation of the graph
+    // For more details, see: https://en.wikipedia.org/wiki/Adjacency_list
     public void printGraph() {
         for (Map.Entry<Integer, List<Integer>> entry : adjacencyList.entrySet()) {
             int vertex = entry.getKey();
@@ -33,9 +41,11 @@ class Graph {
 
 public class ImplementingGraphs {
     public static void main(String[] args) {
-        
+        // Create a graph with 5 vertices
+        // For more details, see: https://en.wikipedia.org/wiki/Graph_theory
         Graph graph = new Graph(5);
 
+        // Add edges
         graph.addEdge(0, 1);
         graph.addEdge(0, 4);
         graph.addEdge(1, 2);
@@ -43,7 +53,8 @@ public class ImplementingGraphs {
         graph.addEdge(1, 4);
         graph.addEdge(2, 3);
         graph.addEdge(3, 4);
-      
+
+        // Print the graph
         graph.printGraph();
     }
 }
