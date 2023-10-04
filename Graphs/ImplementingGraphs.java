5fb3892e36a74fb6dfcce78248eae15163c11560
Shivam Kumar <123649432+shivamk23@users.noreply.github.com>
2023-10-05 00:52:47 +0530

    Update ImplementingGraphs.java
    Added basic description and Wikipedia links.

--- Graphs/ImplementingGraphs.java
+++ Graphs/ImplementingGraphs.java
@@ -1,9 +1,9 @@
 import java.util.*;
 
-
+// Class to represent a graph
 class Graph {
-    private int V;
-    private Map<Integer, List<Integer>> adjacencyList; 
+    private int V; // Number of vertices
+    private Map<Integer, List<Integer>> adjacencyList; // Adjacency list
 
     // Constructor
     public Graph(int V) {
@@ -12,12 +12,19 @@ class Graph {
         for (int i = 0; i < V; i++) {
             adjacencyList.put(i, new ArrayList<>());
         }
-    } 
+    }
+
+    // Add an edge to the graph
+    // This method adds an undirected edge between vertices v and w
+    // For more details, see: https://en.wikipedia.org/wiki/Graph_(discrete_mathematics)
     public void addEdge(int v, int w) {
         adjacencyList.get(v).add(w);
         adjacencyList.get(w).add(v);
     }
+
     // Print the graph
+    // This method prints the adjacency list representation of the graph
+    // For more details, see: https://en.wikipedia.org/wiki/Adjacency_list
     public void printGraph() {
         for (Map.Entry<Integer, List<Integer>> entry : adjacencyList.entrySet()) {
             int vertex = entry.getKey();
@@ -31,11 +38,13 @@ class Graph {
     }
 }
 
-public class ImplementingGraphs {
+public class Main {
     public static void main(String[] args) {
-        
+        // Create a graph with 5 vertices
+        // For more details, see: https://en.wikipedia.org/wiki/Graph_theory
         Graph graph = new Graph(5);
 
+        // Add edges
         graph.addEdge(0, 1);
         graph.addEdge(0, 4);
         graph.addEdge(1, 2);
@@ -43,7 +52,8 @@ public class ImplementingGraphs {
         graph.addEdge(1, 4);
         graph.addEdge(2, 3);
         graph.addEdge(3, 4);
-      
+
+        // Print the graph
         graph.printGraph();
     }
 }
