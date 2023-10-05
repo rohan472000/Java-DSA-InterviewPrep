f5d55a8620ec3211b289eb09d77e8dc973928696
Himateju V <90406478+HimaTeju@users.noreply.github.com>
2023-10-05 09:47:30 +0530

    Added/fibonacci-factorial-with test cases (#86)
    * Moved factorial to math
    
    * Added Test Cases
    
    * added/Fibonacci
    
    * Update MathsforDSA/factorial.java
    
    Co-authored-by: Rohan Anand <96521078+rohan472000@users.noreply.github.com>
    
    * Update MathsforDSA/factorial.java
    
    Co-authored-by: Rohan Anand <96521078+rohan472000@users.noreply.github.com>
    
    * Update MathsforDSA/factorial.java
    
    * Added Condition to check if it is a number
    
    * Update factorial.java
    
    * Update MathsforDSA/factorial.java
    
    * Update MathsforDSA/fibonacci.java
    
    ---------
    
    Co-authored-by: Rohan Anand <96521078+rohan472000@users.noreply.github.com>

---
+++ MathsforDSA/factorial.java
@@ -0,0 +1,28 @@
+// More on Factorial : https://en.wikipedia.org/wiki/Factorial
+
+public class factorial {
+
+    // Driver Code
+    public static void main(String[] args) {
+        tests(); // Call the tests method to run factorial tests
+    }
+
+    //Test Cases 
+    //More on Assert : https://www.geeksforgeeks.org/assertions-in-java/
+    public static void tests() {
+       assert fact(1) == 1;
+       assert fact(5) == 120;
+       assert fact(0) == 1;
+       assert fact ("asg") == 0;
+       System.out.println("Successfully Passed Test Cases!"); // Successfull Message print
+    }
+
+    //Recursive method to calculate Factorial
+    public static int fact(int n) {
+        if(Character.isDigit(n) == false)
+            return 0;
+        if(n == 0)return 1; 
+        return n * fact(n-1);
+    }
+
+}
