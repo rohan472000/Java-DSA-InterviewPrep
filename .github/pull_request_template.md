a70b3da39119d45ab6dce2120869b4f28dcfb869
Priyanka Patel <priya.priyanka1612@gmail.com>
2023-10-05 17:34:33 +0530

    Implemented graph using ArrayList

--- .github/pull_request_template.md
+++ .github/pull_request_template.md
@@ -2,20 +2,17 @@
 ## Pull Request Checkboxes and descriptions
 
 ### Description
-[Provide a concise description of the changes made in this pull request.]
+Implemented graph using arraylist
 
 ### Related/Fixes Issue
-[Reference the related issue or task using its number or link.]
-
-### Changes Made
-[Briefly list the specific code changes made.]
+Issue #67
 
 ### Screenshots (if applicable)
 [Include any relevant screenshots or images here.]
 
-- [ ] Fixed Bug/issues
-- [ ] Added algo/questions_answers/docs
-- [ ] This pull request is all my own work -- I have not plagiarized it.
-- [ ] All filenames are in PascalCase.
-- [ ] All functions and variable names follow Java naming conventions.
-- [ ] All new algorithms have a URL in their comments that points to Wikipedia or other similar explanations.
+- [x] Fixed Bug/issues
+- [x] Added algo/questions_answers/docs
+- [x] This pull request is all my own work -- I have not plagiarized it.
+- [x] All filenames are in PascalCase.
+- [x] All functions and variable names follow Java naming conventions.
+- [x] All new algorithms have a URL in their comments that points to Wikipedia or other similar explanations.
---
+++ Graphs/GraphUsingArrayList.java
@@ -0,0 +1,59 @@
+
+
+//Implementing graph using java
+//Implemented adjacency list using arraylist
+//for undirected unweighted graph
+
+package Graphs;
+
+import java.util.*; 
+
+public class GraphUsingArrayList 
+{
+
+    //storing edges(source and destination) in the list
+    static void addEdge(ArrayList<ArrayList<Integer> > graph, int s, int d) 
+    { 
+        graph.get(s).add(d); 
+        graph.get(d).add(s); 
+    } 
+
+    static void printAdjacencyList(ArrayList<ArrayList<Integer> > graph) 
+    { 
+        for (int i = 0; i < graph.size(); i++) { 
+            //Printing the vertex
+            System.out.println("Adjacency list of " + i); 
+            for (int j = 0; j < graph.get(i).size(); j++) { 
+                //printing neighbours of vertices
+                System.out.print(graph.get(i).get(j) + " "); 
+            } 
+            System.out.println(); 
+        } 
+    } 
+
+    //creating graph by adding edges
+    public static void createGraph(ArrayList<ArrayList<Integer> > graph, int v) 
+    {
+        for (int i = 0; i < v ; i++) 
+            graph.add(new ArrayList<Integer>()); 
+  
+        // adding edges 
+        addEdge(graph, 0, 1); 
+        addEdge(graph, 0, 4); 
+        addEdge(graph, 1, 2); 
+        addEdge(graph, 1, 3); 
+        addEdge(graph, 1, 4); 
+        addEdge(graph, 2, 3); 
+        addEdge(graph, 3, 4); 
+
+    }
+
+    public static void main(String[] args) 
+    { 
+        //creating a graph of 5 vertices
+        int v = 5; 
+        ArrayList<ArrayList<Integer> > graph = new ArrayList<ArrayList<Integer> >(v); 
+        createGraph(graph,v);
+        printAdjacencyList(graph); 
+    }
+}
